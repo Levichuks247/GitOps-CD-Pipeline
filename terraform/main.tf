17c7b58e965c3507db0b16f1ccabe9b761d8670f
Air <air@Air-di-Air.homenet.telecomitalia.it>
2026-03-19 21:00:15 +0100

    fix: add vpc routing to fix node joining failure

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@
 resource "aws_vpc" "gitops_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support   = true # Added for EKS endpoint resolution
   tags                 = { Name = "gitops-vpc" }
 }
 
@@ -9,12 +10,25 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.gitops_vpc.id
 }
 
+# 1. Create a Route Table to allow traffic to the Internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.gitops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
 resource "aws_subnet" "sub_1" {
   vpc_id            = aws_vpc.gitops_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-2a"
   map_public_ip_on_launch = true
-  tags = { "kubernetes.io/cluster/gitops-eks" = "shared" }
+  tags = { 
+    "kubernetes.io/cluster/gitops-eks" = "shared" 
+    "kubernetes.io/role/elb"           = "1" # Helpful for future LoadBalancers
+  }
 }
 
 resource "aws_subnet" "sub_2" {
@@ -22,66 +36,19 @@ resource "aws_subnet" "sub_2" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-2b"
   map_public_ip_on_launch = true
-  tags = { "kubernetes.io/cluster/gitops-eks" = "shared" }
-}
-
-# --- EKS Cluster ---
-resource "aws_eks_cluster" "eks" {
-  name     = "gitops-eks"
-  role_arn = aws_iam_role.cluster.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.sub_1.id, aws_subnet.sub_2.id]
+  tags = { 
+    "kubernetes.io/cluster/gitops-eks" = "shared" 
+    "kubernetes.io/role/elb"           = "1"
   }
 }
 
-# --- Managed Node Group ---
-resource "aws_eks_node_group" "nodes" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "gitops-nodes"
-  node_role_arn   = aws_iam_role.nodes.arn
-  subnet_ids      = [aws_subnet.sub_1.id, aws_subnet.sub_2.id]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  instance_types = ["t3.micro"]
+# 2. Associate the subnets with the Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.sub_1.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-# --- IAM Roles ---
-resource "aws_iam_role" "cluster" {
-  name = "gitops-cluster-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "eks.amazonaws.com" } }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role" "nodes" {
-  name = "gitops-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_worker" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "node_cni" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "node_ecr" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodes.name
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.sub_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
