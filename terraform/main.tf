f1e2e02f51e1f1dc9f0cc5df1e5850f65d2ae20e
Air <air@Air-di-Air.homenet.telecomitalia.it>
2026-03-20 10:20:20 +0100

    fix: rename IAM roles to v2 and upgrade to t3.small

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,25 +70,21 @@ resource "aws_eks_node_group" "nodes" {
   instance_types = ["t3.small"] # Fixed the "Too many pods" error
 }
 
-# --- IAM Roles ---
+# --- IAM Roles (Renamed to v2 to bypass ghost conflicts) ---
 resource "aws_iam_role" "cluster" {
-  name = "gitops-cluster-role"
+  name = "gitops-cluster-role-v2"  # Added -v2
   assume_role_policy = jsonencode({
     Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "eks.amazonaws.com" } }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
 resource "aws_iam_role" "nodes" {
-  name = "gitops-node-role"
+  name = "gitops-node-role-v2"    # Added -v2
   assume_role_policy = jsonencode({
     Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
   })
 }
+
 
 resource "aws_iam_role_policy_attachment" "node_worker" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
